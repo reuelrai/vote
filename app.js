e23c60c0df1953c472e83303338874163c60e651
reuel.rai <149403520+reuelrai@users.noreply.github.com>
2025-01-10 13:03:53 +0545

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ names.forEach(name => {
     option.textContent = name;
     voteSelect.appendChild(option);
 });
+const voteSelect = document.getElementById('name');
+names.forEach(name => {
+    const option = document.createElement('option');
+    option.value = name;
+    option.textContent = name;
+    voteSelect.appendChild(option);
+});
 
 document.getElementById('voteForm').addEventListener('submit', async (event) => {
     event.preventDefault();
